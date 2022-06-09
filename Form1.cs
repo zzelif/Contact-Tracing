84f443686ad7d8db3289443913898adff7695342
Dan Gabriel Lettac <dan.lettac@gmail.com>
2022-06-10 02:37:05 +0800

    second writeline was added and it worked

--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,7 @@ namespace Contact_Tracing
         {
             StreamWriter file = new StreamWriter(@"C:\Users\Public\Documents\Contact Tracing.txt", true);
             file.WriteLine("First Name: " + txtFirstname.Text);
+            file.WriteLine("Middle Name: " + txtMidname.Text);
             file.Close();
             MessageBox.Show("Thank you for filling up this form!");
         }
