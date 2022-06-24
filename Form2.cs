61bcca392ced694a7fc8f4805079058a52c001ff
Dan Gabriel Lettac <dan.lettac@gmail.com>
2022-06-25 00:46:11 +0800

    it worked but an unhandled exception occured

--- Form2.cs
+++ Form2.cs
@@ -27,39 +27,62 @@ namespace Contact_Tracing
 
         private void btnFilterdate_Click(object sender, EventArgs e)
         {
-            StreamReader reader = new StreamReader(@"C:\Users\Public\Documents\Contact Tracing\");
             List<string> dates = new List<string>();
-            string Date = dtpFilter.Value.ToShortDateString();
+            string Date = dtpFilter.Text;
             int dateResults = 0;
-
-            while (!reader.EndOfStream)
+            var txtFiles = Directory.EnumerateFiles(@"C:\Users\Public\Documents\Contact Tracing");
+            foreach (string file in txtFiles)
             {
-                string inputdate = reader.ReadLine();
-                if (inputdate.Contains(Date))
-                {
-                    dateResults++;
-                    dates.Add(inputdate);
-                }
+                string contents = File.ReadAllText(file);
+                if (contents.Contains(Date))
+                    dateResults++; dates.Add(contents); continue;
             }
             if (dateResults == 0)
             {
                 MessageBox.Show("No records found on the selected date");
-                reader.Close();
             }
             else
             {
-                reader.Close();
                 StreamWriter file = new StreamWriter(@"C:\Users\Public\Documents\Contact Tracing\" + dtpFilter.Text + ".txt");
-                foreach (string inputdate in dates)
+                foreach (string contents in dates)
                 {
-                    file.WriteLine(inputdate);
+                    file.WriteLine(contents);
+                    file.Close();
                 }
-                MessageBox.Show("Found " + dateResults.ToString() + " records on the selected date");
-                MessageBox.Show("These will be saved at cam be viewed at Contact Tracing/" + dtpFilter.Text + ".txt");
-                file.Close();
+                MessageBox.Show("Found " + dateResults + " records on the selected date");
+                MessageBox.Show("These will be saved and can be viewed at Contact Tracing folder");
                 Form4 date = new Form4();
                 date.ShowDialog();
             }
+
+            //while (!reader.EndOfStream)
+            //{
+            //    string inputdate = reader.ReadLine();
+            //    if (inputdate.Contains(Date))
+            //    {
+            //        dateResults++;
+            //        dates.Add(inputdate);
+            //    }
+            //}
+            //if (dateResults == 0)
+            //{
+            //    MessageBox.Show("No records found on the selected date");
+            //    reader.Close();
+            //}
+            //else
+            //{
+            //    reader.Close();
+            //    StreamWriter file = new StreamWriter(@"C:\Users\Public\Documents\Contact Tracing\" + dtpFilter.Text + ".txt");
+            //    foreach (string inputdate in dates)
+            //    {
+            //        file.WriteLine(inputdate);
+            //    }
+            //    MessageBox.Show("Found " + dateResults.ToString() + " records on the selected date");
+            //    MessageBox.Show("These will be saved at cam be viewed at Contact Tracing/" + dtpFilter.Text + ".txt");
+            //    file.Close();
+            //    Form4 date = new Form4();
+            //    date.ShowDialog();
+            //}
         }
     }
 }
