27e544f46ea1de03113a2fc65858d0e0f4d97db7
Dan Gabriel Lettac <dan.lettac@gmail.com>
2022-06-24 22:52:21 +0800

    added a label and a button for letter b's function

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.btnShowall = new System.Windows.Forms.Button();
             this.lblShowalldata = new System.Windows.Forms.Label();
+            this.lblFilterdate = new System.Windows.Forms.Label();
+            this.dtpFilter = new System.Windows.Forms.DateTimePicker();
+            this.btnFilterdate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnShowall
@@ -39,26 +42,68 @@
             this.btnShowall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnShowall.Font = new System.Drawing.Font("Berlin Sans FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnShowall.ForeColor = System.Drawing.Color.Cornsilk;
-            this.btnShowall.Location = new System.Drawing.Point(88, 184);
+            this.btnShowall.Location = new System.Drawing.Point(104, 184);
             this.btnShowall.Name = "btnShowall";
             this.btnShowall.Size = new System.Drawing.Size(130, 63);
             this.btnShowall.TabIndex = 1;
-            this.btnShowall.Text = "Show";
+            this.btnShowall.Text = "Continue";
             this.btnShowall.UseVisualStyleBackColor = false;
             this.btnShowall.Click += new System.EventHandler(this.btnShowall_Click);
             // 
             // lblShowalldata
             // 
             this.lblShowalldata.BackColor = System.Drawing.Color.Transparent;
-            this.lblShowalldata.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowalldata.Font = new System.Drawing.Font("Berlin Sans FB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowalldata.ForeColor = System.Drawing.Color.Cornsilk;
             this.lblShowalldata.Image = global::Contact_Tracing.Properties.Resources._9523126451_674b6874d7_b;
             this.lblShowalldata.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.lblShowalldata.Location = new System.Drawing.Point(70, 34);
+            this.lblShowalldata.Location = new System.Drawing.Point(67, 34);
             this.lblShowalldata.Name = "lblShowalldata";
-            this.lblShowalldata.Size = new System.Drawing.Size(168, 147);
+            this.lblShowalldata.Size = new System.Drawing.Size(204, 147);
             this.lblShowalldata.TabIndex = 8;
             this.lblShowalldata.Text = "Show All Data Gathered From Contact Tracing";
             this.lblShowalldata.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblFilterdate
+            // 
+            this.lblFilterdate.BackColor = System.Drawing.Color.Transparent;
+            this.lblFilterdate.Font = new System.Drawing.Font("Berlin Sans FB", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilterdate.ForeColor = System.Drawing.Color.Cornsilk;
+            this.lblFilterdate.Image = global::Contact_Tracing.Properties.Resources._9523126451_674b6874d7_b;
+            this.lblFilterdate.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lblFilterdate.Location = new System.Drawing.Point(708, 34);
+            this.lblFilterdate.Name = "lblFilterdate";
+            this.lblFilterdate.Size = new System.Drawing.Size(204, 78);
+            this.lblFilterdate.TabIndex = 9;
+            this.lblFilterdate.Text = "Filter Record by Date:";
+            this.lblFilterdate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dtpFilter
+            // 
+            this.dtpFilter.CalendarForeColor = System.Drawing.Color.Transparent;
+            this.dtpFilter.CalendarMonthBackground = System.Drawing.Color.Transparent;
+            this.dtpFilter.CalendarTitleBackColor = System.Drawing.Color.Transparent;
+            this.dtpFilter.CalendarTitleForeColor = System.Drawing.Color.Transparent;
+            this.dtpFilter.CalendarTrailingForeColor = System.Drawing.Color.Transparent;
+            this.dtpFilter.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFilter.Location = new System.Drawing.Point(639, 152);
+            this.dtpFilter.Name = "dtpFilter";
+            this.dtpFilter.Size = new System.Drawing.Size(343, 29);
+            this.dtpFilter.TabIndex = 10;
+            // 
+            // btnFilterdate
+            // 
+            this.btnFilterdate.BackColor = System.Drawing.Color.Transparent;
+            this.btnFilterdate.BackgroundImage = global::Contact_Tracing.Properties.Resources._9523126451_674b6874d7_b;
+            this.btnFilterdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnFilterdate.Font = new System.Drawing.Font("Berlin Sans FB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFilterdate.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnFilterdate.Location = new System.Drawing.Point(748, 187);
+            this.btnFilterdate.Name = "btnFilterdate";
+            this.btnFilterdate.Size = new System.Drawing.Size(130, 63);
+            this.btnFilterdate.TabIndex = 11;
+            this.btnFilterdate.Text = "Continue";
+            this.btnFilterdate.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
@@ -67,6 +112,9 @@
             this.BackColor = System.Drawing.Color.Cornsilk;
             this.BackgroundImage = global::Contact_Tracing.Properties.Resources._9523126451_674b6874d7_b;
             this.ClientSize = new System.Drawing.Size(1058, 606);
+            this.Controls.Add(this.btnFilterdate);
+            this.Controls.Add(this.dtpFilter);
+            this.Controls.Add(this.lblFilterdate);
             this.Controls.Add(this.lblShowalldata);
             this.Controls.Add(this.btnShowall);
             this.Name = "Form2";
@@ -79,5 +127,8 @@
 
         private System.Windows.Forms.Button btnShowall;
         private System.Windows.Forms.Label lblShowalldata;
+        private System.Windows.Forms.Label lblFilterdate;
+        private System.Windows.Forms.DateTimePicker dtpFilter;
+        private System.Windows.Forms.Button btnFilterdate;
     }
 }
